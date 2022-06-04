7d95979949ebe95db34e0592fd5126e8dd43c365
hanbanhbao <106421049+hanbanhbao@users.noreply.github.com>
2022-06-04 11:54:59 +0700

    rac

--- data.js
+++
@@ -1,5 +0,0 @@
-var data = [
-    {"ID" : "01" , "Name" : "One Day" , "price" : "500k" },
-    {"ID" : "02" , "Name" : "Visit + Play Games" , "price" : "1000k" },
-    {"ID" : "03" , "Name" : "Visit + Play Games + Drinks" , "price" : "1500k" }
-];
