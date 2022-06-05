5b384390cc9b21b63a515f63832e6e9ac0cbd5bc
hanbanhbao <106421049+hanbanhbao@users.noreply.github.com>
2022-06-05 17:18:13 +0700

    book ticket and restaurant

--- js/ticket.js
+++ js/ticket.js
@@ -2,20 +2,13 @@ $(document).ready(function () {
   $("#reserve").click(function () {
     $("#menuSelect").toggle("slow", "swing");
   });
-  $("#buyNow").click(function () {
-    alert("One");
-  });
   $("#buyNow1").click(function () {
     $("#menuSelect").hide();
-  });
-  $("#young").select(function () {
-    alert("Handler for .select() called.");
   });
 });   
 var price = 150;
 var amountDT = 1;
 var dayDT = 1;
-var all = 1;
 document.getElementById("price").innerText = '$' + price;
 
 function changeSl() {
@@ -42,19 +35,19 @@ function changeSl() {
   }
 
   var myButton = document.getElementById("myBtn");
-  window.onscroll = function() {scrollFunction()};
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 
-      || document.documentElement.scrollTop > 20) {
-        myButton.style.display = "block";
-    } else {
-      myButton.style.display = "none";
-    }
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+  if (document.body.scrollTop > 20
+    || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
   }
-  function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
   
 
 
