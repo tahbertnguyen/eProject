4a11d8a59adb55848e925e7a690ae19a8138f9c6
hanbanhbao <106421049+hanbanhbao@users.noreply.github.com>
2022-06-05 17:18:29 +0700

    book ticket and restaurant

--- js/ticket.js
+++ js/ticket.js
@@ -2,10 +2,25 @@ $(document).ready(function () {
   $("#reserve").click(function () {
     $("#menuSelect").toggle("slow", "swing");
   });
+  $("#buyNow").click(function () {
+    $("#showSure").show("slow", "swing");
+  });
+  $("#hideSure").click(function () {
+    $("#showSure").hide("slow", "swing");
+  });
   $("#buyNow1").click(function () {
     $("#menuSelect").hide();
   });
-});   
+  $(".addRes").click(function () {
+    $("#addRes1").show("slow", "swing");
+  });
+  $("#hideaddRes").click(function () {
+    $("#addRes1").hide("slow", "swing");
+  });
+});
+var all = 1;
+document.getElementById("addRes2").innerText = 
+'Successful restaurant booking !' + '\n' + 'Please pay in the booking tickets !'
 var price = 150;
 var amountDT = 1;
 var dayDT = 1;
@@ -14,27 +29,35 @@ document.getElementById("price").innerText = '$' + price;
 function changeSl() {
   var dataSl = document.getElementById("human").value;
   if (dataSl == 'Child') {
-     price = 150;
+    price = 150;
   } else if (dataSl == 'Young') {
-     price = 200;
+    price = 200;
   } else {
-     price = 300;
+    price = 300;
   }
   document.getElementById("price").innerText = '$' + price;
   all = price * amountDT * dayDT;
-  document.getElementById("all").innerText ='$' + all;
-  document.getElementById("intro").innerText ='Total money';
+  document.getElementById("all").innerText = '$' + all;
+  document.getElementById("intro").innerText = 'Total money';
 }
 
-  function nbTicket(){
-    amountDT = document.getElementById("amount").value;
-    dayDT = document.getElementById("day").value;
-    all = price * amountDT * dayDT;
-    document.getElementById("intro").innerText ='Total money';
-    document.getElementById("all").innerText ='$' + all;
-  }
+document.getElementById("showSure1").innerText = 'Please complete all information !'
+function nbTicket() {
+  amountDT = document.getElementById("amount").value;
+  dayDT = document.getElementById("day").value;
+  all = price * amountDT * dayDT;
+  document.getElementById("intro").innerText = 'Total money';
+  document.getElementById("all").innerText = '$' + all;
+  if (all > 1) {
+    document.getElementById("showSure1").innerText = 'Successful ticket set !'
+                  + '\n' + '$' + all;
+    }
+    else {
+      document.getElementById("showSure1").innerText = 'Please complete all information !'
+    }
+}
 
-  var myButton = document.getElementById("myBtn");
+var myButton = document.getElementById("myBtn");
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
   if (document.body.scrollTop > 20
@@ -48,7 +71,8 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-  
+
+
 
 
 
