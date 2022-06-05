23aab64b9d70d584f2ad50170bd29c3284d9ca4d
hanbanhbao <106421049+hanbanhbao@users.noreply.github.com>
2022-06-05 13:27:24 +0700

    service

--- js/ticket.js
+++ js/ticket.js
@@ -13,13 +13,8 @@ $(document).ready(function () {
   });
 });
 
-
-document.getElementById("dataSl").innerText = 'Child';
-dataSl = 'Child';
-document.getElementById("price").innerText = '$' + 150;
 function changeSl() {
   var dataSl = document.getElementById("human").value;
-  document.getElementById("dataSl").innerText = dataSl;
   if (dataSl == 'Child') {
     var price = 150;
   } else if (dataSl == 'Young') {
@@ -30,14 +25,8 @@ function changeSl() {
   document.getElementById("price").innerText = '$' + price;
 }
 
-  var amountDT = 1;
-  document.getElementById("amountDT").innerText = amountDT;
-  var dayDT = 1;
-  document.getElementById("dayDT").innerText = dayDT;
-
   function nbTicket(){
     amountDT = document.getElementById("amount").value;
-    document.getElementById("amountDT").innerText = amountDT;
     dayDT = document.getElementById("day").value;
     document.getElementById("dayDT").innerText = dayDT;
   }
